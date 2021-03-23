dcc42e807c595bce046e6863131c2aaad6e3a6e3
Fabian0121 <fabianjuarezsandoval@gmail.com>
2021-03-23 01:27:01 -0600

    Euler Mejorado Final

--- src/metodos/runge.java
+++ src/metodos/runge.java
@@ -2,6 +2,7 @@
 package metodos;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 
 public class runge {
@@ -52,20 +53,78 @@ public class runge {
         //
         double valor =1;
         Va[0]= valor;
-        //
-        double b1=2;
+        //valored de k;
+        BigDecimal aux1,aux2;
+        BigDecimal a1,a2,a3,a4,a5,h,constante,c2,c3,k1,k2,k3,k4;
+        double b;
         double redondeado;
         for (int i = 1; i < Xn.length; i++) {
-            //Sumas y multiplicacion
-            a1=b1*Va[i-1]*Xn[i-1];
-            redondeado =redondear.redondeo4(a1);
-            
-            a2=0.025*redondeado;
-            redondeado =redondear.redondeo4(a2);
-
-            a3=Va[i-1]+redondeado;
-            redondeado = redondear.redondeo4(a3);
+            //Valores de K1
+            //K1=hf(Xn-Yn)
+            constante=new BigDecimal(2);
+            h = new BigDecimal("0.025");
+            aux1=new BigDecimal(Xn[i-1]);
+            aux2=new BigDecimal(Va[i-1]);
+            a1=aux1.multiply(aux2);
+            a2=a1.multiply(constante).multiply(h);
+            b= a2.floatValue();
+            redondeado = redondear.redondeo6(b);
+            a2 = new BigDecimal(redondeado);
+            k1=a2;
+            //K2=hf(xn-1+1/2h)(yn-1+1/2K1)
+            c2 = new BigDecimal("0.5");
+            a1=c2.multiply(h);
+            a2=a1.add(aux1);
+            //
+            a1=c2.multiply(k1);
+            a3=a1.add(aux2);
+            a4=a2.multiply(a3);
+            //
+            a5=a4.multiply(h);
+            b= a5.floatValue();
+            redondeado = redondear.redondeo6(b);
+            a5 = new BigDecimal(redondeado);
+            k2=a5;
+            //K3 hf(xn-1+1/2h)(yn-1+1/2h)
+            c2 = new BigDecimal("0.5");
+            a1=c2.multiply(h);
+            a2=a1.add(aux1);
+            //
+            a1=c2.multiply(k2);
+            a3=a1.add(aux2);
+            a4=a2.multiply(a3);
+            //
+            a5=a4.multiply(h);
+            b= a5.floatValue();
+            redondeado = redondear.redondeo6(b);
+            a5 = new BigDecimal(redondeado);
+            k3=a5;
+            //K4 hf(Xn-1+h)(Yn-1+K3)
+            a1=aux1.add(h);
+            a2=aux2.add(k3);
+            a3=a1.multiply(a2);
+            a4=h.multiply(a3);
+            b= a4.floatValue();
+            redondeado = redondear.redondeo6(b);
+            a4 = new BigDecimal(redondeado);
+            k4=a4;
+            //Y Yn-1-1/6[k1+2k2+2k3+k4]
+            a1=constante.multiply(k2);    
+            a2=constante.multiply(k3);
+            a3=k1.add(a1).add(a2).add(k4);
+            //
+            BigDecimal a,d,e;
+            c3= new BigDecimal(0.16666666666);
+            a4=c3.multiply(a3);
+            b= a4.floatValue();
+            redondeado = redondear.redondeo10(b);
+            a4= new BigDecimal(redondeado);
+            a5=aux2.add(a4);
+            b= a5.floatValue();
+            redondeado = redondear.redondeo4(b);
             Va[i]=redondeado;
+            
+            
         }
         return Va;
     }

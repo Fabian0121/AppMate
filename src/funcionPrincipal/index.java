2bb048e3ab4d7033fa5634ee74969b95fb97f7e9
Fabian0121 <fabianjuarezsandoval@gmail.com>
2021-03-23 12:57:15 -0600

    index

--- src/funcionPrincipal/index.java
+++ src/funcionPrincipal/index.java
@@ -9,36 +9,9 @@ public class index {
 
     public static void main(String[] args) {
         eulerN Metodos = new eulerN();
-        //Inicio inicio = new Inicio();
-        //inicio.setVisible(true);
-        metodos.runge metodo= new metodos.runge();
-        //
-        double num[]=metodo.calcularx();
-        double num2[]=metodo.calcularY();
-        double num3[]=metodo.valorReal();
-        double num4[]=metodo.error();
-        double num5[]=metodo.errorRelativo();
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
         
-        
-        for (int i = 0; i < 20; i++) {
-            System.out.println(num2[i]);
-        }
-        //
-        /*BigDecimal num[]=Metodos.calcularx();
-        for (int i = 0; i < num.length; i++) {
-            System.out.println(" ");
-            System.out.println(num[i]);
-            System.out.println(" ");
-        }*/
     }
-    /*La responsabilidad social es aquella que marca prfundamente al ser humano a un nivel
-    //Responsabilidad social es la forma de responder a la sociedad de manera general
-    Onu aquella conciencia con nuestras deciciones que nos 
-    Aque lla concicneicia con el impacto de nuestras decisiones que tendrean impracto en un futuro
-    Los tipos de responsabilidad
-    1-Responsabilidad social empresarial, se refiere al compromiso conciente y concruente de cumplir con los comprisos de la empresa
-    2-Responsabilidad social ambiental se refiere a la imputalidad de una valoracion negativa o positiva de una decision
-    3- Rsponsabilida social o universitaria con la finalidad d etener ideas creativas y comprometidos con la solucion de problemas ambientales
-    */
     
 }
